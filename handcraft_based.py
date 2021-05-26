98dc7b18079d9b8c9ff62ca0ba880914081b263d
miocute <pongthebigbear@gmail.com>
2021-05-26 19:47:40 +0700

    Update handcraft_based.py

--- handcraft_based.py
+++ handcraft_based.py
@@ -1,15 +1,3 @@
-import glob
-
-import cv2
 import numpy as np
-import tqdm as t
-import sklearn.svm as svm
-from joblib import load, dump
-import matplotlib.pyplot as plt
-import os
-
-IMAGE_SIZE = (64, 128)
-
-Train_PATH = "./Image_Dataset/Train/"
-Test_PATH = "./Image_Dataset/Test/"
-
+import tensorflow as tf
+from tensorflow import keras
