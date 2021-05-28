38e8067ee34897d9b99ce39ba262879e67ec9def
Time <time30569@gmail.com>
2021-05-28 11:30:39 +0700

    update

--- learning_based.py
+++ learning_based.py
@@ -8,46 +8,46 @@ from torch.nn import CrossEntropyLoss
 import torch
 from tqdm import tqdm
 import os
+# for someone error
 import collections
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-TEST_ROOT = 'dataset/Tr/emoji'  # path for test datasets
-TRAIN_ROOT = 'dataset/Tr/emoji'  # path for train datasets
+
+# directory
+TEST_ROOT = 'dataset/Tr/'  # path for test datasets
+TRAIN_ROOT = 'dataset/Tr/'  # path for train datasets
 PATH = './GoogLeNet.pth'  # path for load model
 
-PRETRAINED_SIZE = 224  # define pretrained size
-PRETRAINED_MEANS = [0.485, 0.456, 0.406]  # define pretrained means
-PRETRAINED_STDS = [0.229, 0.224, 0.225]  # define pretrained stds
-TRAIN_TRANSFORMS = transforms.Compose([  # define transforms for train datasets
-        transforms.Resize(PRETRAINED_SIZE),
+# GoogLeNet Preset
+TRAIN_PREPROCESS = transforms.Compose([  # define transforms for train datasets
+        transforms.Resize(256),
         transforms.RandomRotation(5),
         transforms.RandomHorizontalFlip(0.5),
-        transforms.RandomCrop(PRETRAINED_SIZE, padding=10),
+        transforms.RandomCrop(224, padding=10),
         transforms.ToTensor(),
-        transforms.Normalize(mean=PRETRAINED_MEANS, std=PRETRAINED_STDS)])
-TEST_TRANSFORMS = transforms.Compose([  # define transforms for test datasets
-        transforms.Resize(PRETRAINED_SIZE),
-        transforms.CenterCrop(PRETRAINED_SIZE),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+TEST_PREPROCESS = transforms.Compose([  # define transforms for test datasets
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=PRETRAINED_MEANS, std=PRETRAINED_STDS)])
-BATCH_SIZE = 40  # define batch size
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-def run():
+def main():
     model = GoogLeNet(aux_logits=False)
     criterion = CrossEntropyLoss()
     optimizer = SGD(model.parameters(), lr=0.001, momentum=0.9)
+    # move the input and model to GPU for speed if available
     if torch.cuda.is_available():
         model = model.cuda()
         criterion = criterion.cuda()
-    
-    # train model
-    train_data = datasets.ImageFolder(root=TRAIN_ROOT, transform=TRAIN_TRANSFORMS)
-    train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
-    """Function to train the model"""
-    for epoch in tqdm(range(25)):  # loop over the dataset multiple times
+
+    train_data = datasets.ImageFolder(root=TRAIN_ROOT, transform=TRAIN_PREPROCESS)
+    train_loader = DataLoader(train_data, batch_size=40, shuffle=True, num_workers=0)
+    # Loop training the model
+    for epoch in tqdm(range(35)):  # loop over the dataset 35 times
         running_loss = 0.0
         model.train()
         for i, data in enumerate(train_loader, 0):
-            # get the inputs; data is a list of [inputs, labels]
+            # get the inputs then list in input and labels
             inputs, labels = data
             if torch.cuda.is_available():
                 inputs = inputs.cuda()
@@ -59,21 +59,19 @@ def run():
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
-            # print statistics
             running_loss += loss.item()
+            # print statistics
             if i % 100 == 0:  # print every 100 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
     print('Finished Training')
     torch.save(model.state_dict(), PATH)
-    # test model
+
+    # Testing the model
     model.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
-    test_data = datasets.ImageFolder(root=TEST_ROOT, transform=TEST_TRANSFORMS)
-    test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
-    print('start testing model...')
-    """Function to test the model"""
+    test_data = datasets.ImageFolder(root=TEST_ROOT, transform=TEST_PREPROCESS)
+    test_loader = DataLoader(test_data, batch_size=40, shuffle=True, num_workers=0)
     classes = test_data.classes
     correct_pred = {classname: 0 for classname in classes}
     total_pred = {classname: 0 for classname in classes}
@@ -86,8 +84,7 @@ def run():
                 labels = labels.cuda()
             outputs = model(images)
             _, predictions = torch.max(outputs, 1)
-            # collect the correct predictions for each class
-            
+            # collect the correct predictions
             n_images = len(images)
             rows = int(np.sqrt(n_images))
             cols = int(np.sqrt(n_images))
@@ -114,6 +111,6 @@ def run():
     print(od)
     for classname, correct_count in od.items():
         accuracy = 100 * float(correct_count) / total_pred[classname]
-        print("Accuracy for class {:5s} is: {:.1f} %".format(classname,accuracy))
+        print("Accuracy of class {:5s} = {:.1f} %".format(classname,accuracy))
 
-run()
+main()
